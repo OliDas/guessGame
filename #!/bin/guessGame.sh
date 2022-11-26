fa4991466660832f122cec11c86e50be470027c5
Oli Das <olihemshella@gmail.com>
2022-11-26 04:53:19 -0500

    Create guessGame.sh

---
+++ #!/bin/guessGame.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+guessGame() {
+    numCorrect=$(ls -A|wc -l)
+    echo "Hi User! Can you guess the number of files present in the current working directory?"
+    while true;
+    do
+        read  numGuessed
+        if [ $numGuessed -lt $numCorrect ]
+        then
+            echo "Try Again! The guessed number is lesser than the actual number."
+        elif [ $numGuessed -gt $numCorrect ]
+        then
+            echo "Try Again! The guessed number is greater than the actual number."
+        else
+            echo " Congratulations !!! Your guess is right!"
+		break;
+        fi
+	done
+}
+guessGame
